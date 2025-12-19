23319ccdbcf51be12b910f3a6db9c01720e8d0fa
Nandani Khalas <nandani.khalas@lectus.tech>
2025-12-19 16:39:03 +0530

    npm run start is fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,24 @@
-const { rules } = require("eslint-plugin-react");
-
-module.exports={
-    env: {
-        browser:true,
-        node:true,
-        es2024:true,
+module.exports = {
+  env: {
+    node: true,      // 👈 tells ESLint that 'module' and 'require' are valid
+    es2021: true,
+    browser: true,   // keep if you lint frontend code too
+  },
+  settings: {
+    react: {
+      version: "detect", // 👈 fixes React version warning
     },
-    extends:["eslint:recommended"],
-}
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    // custom rules if needed
+  },
+};
